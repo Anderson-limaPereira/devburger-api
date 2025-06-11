af11c8592ea39f0bb2f5b8d99d61a85deea6d802
@Anderson-limaPereira <161944582+Anderson-limaPereira@users.noreply.github.com>
2025-06-10 22:05:44 -0300

    atualizando database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,16 +1,30 @@
 
-const configDatabase = {
-    dialect: 'postgres', 
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'devburger',
-    define: {
-      timestamps: true,
-      underscored: true,
-      underscoredAll: true,
-    },
-  };
+// const configDatabase = {
+//     dialect: 'postgres', 
+//     host: 'localhost',
+//     port: 5432,
+//     username: 'postgres',
+//     password: 'postgres',
+//     database: 'devburger',
+//     define: {
+//       timestamps: true,
+//       underscored: true,
+//       underscoredAll: true,
+//     },
+//   };
   
-  export default configDatabase; 
+//   export default configDatabase; 
+
+export default {
+  dialect: 'postgres',
+  host: process.env.PG_HOST || 'localhost',
+  username: process.env.PG_USER || 'postgres',
+  password: process.env.PG_PASSWORD || '',
+  database: process.env.PG_DATABASE || 'devburger',
+  port: process.env.PG_PORT || 5432,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
