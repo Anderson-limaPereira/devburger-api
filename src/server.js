95d7249c15b7e1628386285859b88b14e0ae3eeb
@Anderson-limaPereira <161944582+Anderson-limaPereira@users.noreply.github.com>
2025-06-10 22:55:27 -0300

    voltando as configurações originais do projeto

--- src/server.js
+++ src/server.js
@@ -1,12 +1,4 @@
 
-// import app from './app.js'
+import app from './app.js'
 
-// app.listen(3001, () => console.log('server is running at port 3001...'))
-
-import app from './app.js';
-
-const PORT = process.env.PORT || 3001;
-
-app.listen(PORT, () => {
-  console.log(`Servidor rodando na porta ${PORT}...`);
-});
+app.listen(3001, () => console.log('server is running at port 3001...'))
